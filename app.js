b23cc67f577cb57b01cd70f9206a5208bbe4d4cc
SKG360 <samuel.giles@colorado.edu>
2020-05-19 09:57:55 -0600

    adds bcrypt encryption technology

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const ejs = require('ejs');
-const md5 = require('md5');
+// const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -39,23 +41,26 @@ app.get('/register', function(req, res) {
 });
 
 app.post('/register', function(req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
 
-  newUser.save(function(err) {
-    if (err) {
-      console.log(err)
-    } else {
-      res.render('secrets') // sends user to secrets page (only after successful login)
-    }
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+
+    newUser.save(function(err) {
+      if (err) {
+        console.log(err)
+      } else {
+        res.render('secrets') // sends user to secrets page (only after successful login)
+      }
+    });
   });
 });
 
 app.post('/login', function(req, res) {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({email: username}, function(err, foundUser) {
     // 'username' comes from user trying to log in (email addy)
@@ -63,11 +68,12 @@ app.post('/login', function(req, res) {
       console.log(err);
     } else {
       if (foundUser) { // if username matches email
-        if (foundUser.password === password) {
-          res.render('secrets');
-        } else {
-          console.log(err);
-        }
+        // if (foundUser.password === password) {
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result === true) {
+            res.render('secrets');
+          }
+        });
       }
     }
   });
